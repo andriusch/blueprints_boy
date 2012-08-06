86eddf1ddbd74fffd9c84fa821f7ca4d77a961f3
Andrius Chamentauskas <andrius.chamentauskas@gmail.com>
2012-08-06 17:11:33 +0300

    Fixed deprecation error

--- integration/active_record/setup.rb
+++ integration/active_record/setup.rb
@@ -2,6 +2,6 @@ require 'active_record'
 ActiveRecord::Base.establish_connection(adapter: 'mysql2', database: 'blueprints_boy_test')
 
 class ARFruit < ActiveRecord::Base
-  set_table_name 'fruits'
+  self.table_name = 'fruits'
   attr_protected :species
 end
