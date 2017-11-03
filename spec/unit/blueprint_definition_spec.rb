7d79b6af77f2e7fd5cea27e347db883447c4101b
Andrius Chamentauskas <andrius.chamentauskas@gmail.com>
2017-11-04 00:20:03 +0200

    Name and factory class don't need to be frozen

--- spec/unit/blueprint_definition_spec.rb
+++ spec/unit/blueprint_definition_spec.rb
@@ -3,10 +3,10 @@ require 'spec_helper'
 describe BlueprintsBoy::Blueprint do
   describe '#initialize' do
     specify do
-      subject.name.should be_nil.and be_frozen
+      subject.name.should be_nil
       subject.dependencies.should eq([]).and be_frozen
       subject.attributes.should eq({}).and be_frozen
-      subject.factory_class.should be_nil.and be_frozen
+      subject.factory_class.should be_nil
       subject.strategies.should match(attributes: instance_of(Proc)).and be_frozen
     end
   end
